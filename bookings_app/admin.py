bde4a1a5bc8450f23621e2879015e572fc2e2c34
miles <miles.pickull@web.de>
2025-10-30 23:54:27 +0100

    feat(admin-panel): registered Models for Admin Panel View

--- bookings_app/admin.py
+++ bookings_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Participant, Booking
+
+admin.site.register(Participant)
+admin.site.register(Booking)
--- events_app/admin.py
+++ events_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import EventCategory, Location, Event
+
+admin.site.register(EventCategory)
+admin.site.register(Location)
+admin.site.register(Event)
