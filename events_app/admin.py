3cb06a71abc00cb42cf96fd7f4946134ca108b71
miles <miles.pickull@web.de>
2025-10-31 00:51:35 +0100

    feat(EventModel): added fieldsets for EventAdmin Model, added Function to set fields for Organisation as not requiered

--- events_app/admin.py
+++ events_app/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
-
 from .models import EventCategory, Location, Event
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'location', 'date')
     list_filter = ('category', 'location', 'date')
     search_fields = ('title',)
+
+    fieldsets = (
+        ('Allgemein', {
+            'fields': ('title', 'category', 'date'),
+        }),
+        ('Organisation', {
+            'classes': ('collapse',),
+            'fields': ('location', 'capacity'),
+        }),
+    )
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        for fname in ('location', 'capacity'):
+            if fname in form.base_fields:
+                form.base_fields[fname].required = False
+        return form
 
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('name', 'address')
